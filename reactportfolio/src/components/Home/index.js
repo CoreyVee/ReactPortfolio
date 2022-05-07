e809f2e4a8487c7725fab8b8ef19d38fd9842f55
CoreyVee <corey.villanueva@gmail.com>
2022-05-07 16:41:14 -0500

    added homepage and animated letters effect

--- reactportfolio/src/components/Home/index.js
+++ reactportfolio/src/components/Home/index.js
@@ -1,21 +1,77 @@
-import './index.scss';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-c.png'
-
+import './index.scss'
 
 const Home = () => {
-    return (
-    <div className="container home-page">
-        <h1>Hi, <br/> I'm
-        <img src={LogoTitle} alt='developer'></img>
-        orey
-        <br/>
-        Full-Stack Software Engineer
-        </h1>
-        <h2>Full Stack developer/ Agile enthusiast/ Life-long learner</h2>
-        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-    </div>
-    )
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['o', 'r', 'e', 'y']
+  const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'E',
+    'n',
+    'g',
+    'i',
+    'n',
+    'e',
+    'e',
+    'r',
+    '.'
+  ]
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Home
